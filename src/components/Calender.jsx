033cdfc7c6d467ec5226b9b5540c5ba3ebbf2eff
kineshlohar <kineshmalviya@gmail.com>
2023-10-28 16:37:20 +0530

    adding comments

--- src/components/Calender.jsx
+++ src/components/Calender.jsx
@@ -13,15 +13,19 @@ const Calendar = () => {
   const [startHour, setStartHour] = useState(8);
   const [endHour, setEndHour] = useState(23);
 
-  console.log(timeZone)
 
+  // handle date change from Datepicker component
   const handleDateChange = (date) => {
     setSelectedDate(date);
   };
 
+  // handle timezone change from dropdown menu
   const handleTimeZoneChange = (e) => {
     const currentTimeZone = e.target.value;
     setTimeZone(currentTimeZone);
+
+    //changing working times based on timezones
+
     if(currentTimeZone === "UTC+9:30"){
       setStartHour(10);
       setEndHour(21);
@@ -35,15 +39,17 @@ const Calendar = () => {
     }
   };
 
+  // changing to next week from next week button on top bar
   const goToNextWeek = () => {
     setSelectedDate(moment(selectedDate).add(1, 'week').toDate());
   };
 
+  // changing to prev week from prev week button on top bar
   const goToPreviousWeek = () => {
     setSelectedDate(moment(selectedDate).subtract(1, 'week').toDate());
   };
 
-  // Define a function to generate time slots from 8:00 AM to 11:00 PM with 30-minute intervals.
+  // Define a function to generate time slots with 30-minute intervals.
   const generateTimeSlots = () => {
 
     const timeSlots = [];
@@ -58,6 +64,8 @@ const Calendar = () => {
     return timeSlots;
   };
 
+
+  
   useEffect(() => {
     const filteredTimeSlots = generateTimeSlots();
     setTimeSlots(filteredTimeSlots);
@@ -67,11 +75,14 @@ const Calendar = () => {
   return (
     <div>
       <div>
+        {/* Top Bar */}
         <div className='h-[4rem] w-[100%] p-4 bg-slate-100 flex items-center justify-start'>
           <button onClick={goToPreviousWeek}>Previous Week</button>
           <DatePicker selected={selectedDate} onChange={handleDateChange} dateFormat="yyyy-MMM-dd" className='mx-6 p-2' />
           <button onClick={goToNextWeek} className=''>Next Week</button>
         </div>
+
+        {/* Drop Down List */}
         <div className='p-2 '>
           <h3 className=' text-lg font-semibold'>Timezone :</h3>
           <select value={timeZone} onChange={handleTimeZoneChange} className='w-[100%] p-2 border-2 border-gray-600'>
@@ -80,19 +91,28 @@ const Calendar = () => {
             <option value="UTC-8">[UTC-8] Pacific Standard Time</option>
           </select>
         </div>
+
       </div>
+
       <div>
+
         <div className="border-2">
+
           <div className='flex flex-col items-start justify-start'>
             {Array.from({ length: 7 }).map((_, dayIndex) => {
               const currentDateForDay = moment(selectedDate).add(dayIndex, 'days');
               const isPast = currentDateForDay < currentDate;
+
               return (
+                // div that contains day and date on left and times with checkboxes on right
                 <div className='flex w-[100%]' key={dayIndex}>
+
                   <div className="p-4 w-36 flex flex-col items-center justify-center bg-slate-200">
                     <h2 className=' text-red-600 font-medium'>{moment(currentDateForDay).format('ddd')}</h2>
                     <p>{moment(currentDateForDay).format('MMM D')}</p>
                   </div>
+
+                  {/* check if the date is passed or is weekend if not then shows checkboxes */}
                   <div className="p-4 flex items-center justify-start flex-wrap">
                     {isPast ? (
                       <label className="bg-gray-300 p-2 rounded-md">Past</label>
@@ -100,8 +120,14 @@ const Calendar = () => {
                       <label className="bg-red-300 p-2 rounded-md">Weekend</label>
                     ) : (
                       timeSlots.map((time, index) => {
+
+
                         const timeSlotId = `time-slot-${index}-${dayIndex}`;
+
+
                         const currentDateForDayFormatted = moment(currentDateForDay).format('YYYY-MM-DD');
+
+                        // check if the checkbox data is there in mockjson data to check the box by default , returns true or false
                         const isChecked = data.some((item) => {
                           const dateMatches = moment(item.Date).isSame(currentDateForDayFormatted, 'day');
                           const timeMatches = item.Time === time;
@@ -109,6 +135,7 @@ const Calendar = () => {
                         });
 
                         return (
+                          // return checkboxes with times day
                           <div key={index} className='m-2'>
                             <div>
                               <input type="checkbox" id={timeSlotId} defaultChecked={isChecked} className='mx-2'/>
@@ -122,6 +149,7 @@ const Calendar = () => {
 
                 </div>
               );
+
             })}
           </div>
         </div>
